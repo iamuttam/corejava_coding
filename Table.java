ca124e08a25dddfeb82964141b3c797aa8566b88
iamuttam <48365184+iamuttam@users.noreply.github.com>
2019-03-24 16:44:42 +0530

    Create table of any no.

---
+++ Table.java
@@ -0,0 +1,15 @@
+package com;
+import java.util.Scanner;
+public class Table {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter  n value want for the Table Creartion:");
+		int n=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+"="+(n*i));
+		}
+
+	}
+
+}
