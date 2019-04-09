443151bd6d460af7cc5ccdcbe3cefb39b3fa652a
iamuttam <48365184+iamuttam@users.noreply.github.com>
2019-04-09 13:09:59 +0530

    Element in Descending Order.

---
+++ Array_Descending.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Array_Descending {
+
+	public static void main(String[] args) {
+		int temp=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no of Elements");
+		int n=sc.nextInt();
+		System.out.println("Enter the Elements.");
+		int a[]= new int [n];
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(a[i]<a[j]) {
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;				
+				}
+			}
+		}
+		System.out.println("Descending Order.");
+		for(int i=0;i<n-1;i++) {
+			System.out.print(a[i]+",");
+		}
+		System.out.println(a[n-1]);
+	}
+}
